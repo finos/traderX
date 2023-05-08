f9bbf7269b76497c49eddff10d976a54f5e34ed3
DovOps <dovops@noreply.users.github.com>
2023-05-08 09:09:07 +0300

    #9 Added auto-redirect to swagger UI, added better description to OpenAPI Spec Generator, Organized Imports

--- trade-processor/src/main/java/finos/traderx/tradeprocessor/OpenApiConfig.java
+++ trade-processor/src/main/java/finos/traderx/tradeprocessor/OpenApiConfig.java
@@ -1,23 +1,37 @@
 package finos.traderx.tradeprocessor;
-import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.OpenAPI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
-@Bean
-public OpenAPI config() {
-    return new OpenAPI()
-            .addServersItem(serverInfo());
-}
+    @Value("${server.port}")
+    private int port = 18091;
 
-private Server serverInfo() {
-    return new Server()
-            .url("");
-}
+    @Bean
+    public OpenAPI config() {
+        Info info = new Info()
+                .title("FINOS TraderX Trading Processor")
+                .version("0.1.0")
+                .description("Service for processing trades from the Trade Feed, Persisting trades, updating positions, and Publishing updates to the feed");
+
+        OpenAPI api = new OpenAPI()
+                .addServersItem(serverInfo("", "Empty URL to help proxied documentation work"))
+                .addServersItem(serverInfo("http://localhost:" + port, "Local Dev URL"));
+
+        api.setInfo(info);
+        return api;
+    }
+
+    private Server serverInfo(String url, String desc) {
+        return new Server()
+                .description(desc)
+                .url(url);
+    }
 }
--- trade-processor/src/main/java/finos/traderx/tradeprocessor/controller/TradeServiceController.java
+++ trade-processor/src/main/java/finos/traderx/tradeprocessor/controller/TradeServiceController.java
@@ -1,29 +1,18 @@
 package finos.traderx.tradeprocessor.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import finos.traderx.tradeprocessor.model.*;
+import finos.traderx.tradeprocessor.model.TradeBookingResult;
+import finos.traderx.tradeprocessor.model.TradeOrder;
 import finos.traderx.tradeprocessor.service.TradeService;
-import io.swagger.models.Response;
 
 @CrossOrigin("*")
 @RestController
