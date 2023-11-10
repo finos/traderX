47d1d0ebf0a7f87a72f4f1a1c387c542ef12225f
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-11-10 12:46:27 +0000

    trade-processor: removed unused imports

--- trade-processor/src/main/java/finos/traderx/tradeprocessor/service/TradeService.java
+++ trade-processor/src/main/java/finos/traderx/tradeprocessor/service/TradeService.java
@@ -1,15 +1,11 @@
 package finos.traderx.tradeprocessor.service;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import finos.traderx.messaging.PubSubException;
 import finos.traderx.messaging.Publisher;
