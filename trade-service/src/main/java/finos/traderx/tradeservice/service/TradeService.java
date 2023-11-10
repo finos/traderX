875bc6cadc1ac0b18b0f8616df48370b92ce20f7
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-11-10 11:36:11 +0000

    trade-service: added the validateticker method

--- trade-service/src/main/java/finos/traderx/tradeservice/service/TradeService.java
+++ trade-service/src/main/java/finos/traderx/tradeservice/service/TradeService.java
@@ -1,4 +1,44 @@
 package finos.traderx.tradeservice.service;
 
+import finos.traderx.tradeservice.controller.TradeOrderController;
+import finos.traderx.tradeservice.model.Security;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class TradeService {
+    private static final Logger log = LoggerFactory.getLogger(TradeOrderController.class);
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Value("${reference.data.service.url}")
+    private String referenceDataServiceAddress;
+    @Value("${account.service.url}")
+    private String accountServiceAddress;
+
+    public boolean validateTicker(String ticker)
+    {
+        // Move whole method to a sperate class that handles all reference data
+        // so we can mock it and run without this service up.
+        String url = this.referenceDataServiceAddress + "//stocks/" + ticker;
+        ResponseEntity<Security> response = null;
+
+        try {
+            response = this.restTemplate.getForEntity(url, Security.class);
+            log.info("Validate ticker " + response.getBody().toString());
+            return true;
+        }
+        catch (HttpClientErrorException ex) {
+            if (ex.getRawStatusCode() == 404) {
+                log.info(ticker + " not found in reference data service.");
+            }
+            else {
+                log.error(ex.getMessage());
+            }
+            return false;
+        }
+    }
 }
