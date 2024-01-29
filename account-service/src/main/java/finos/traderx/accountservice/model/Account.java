3ae0eb4fea437722b2f1b061577a897453534dd3
dovkatz <Dov.Katz@morganstanley.com>
2024-01-29 10:23:33 +0000

    #103 #104 - Address account service bug and fix swaggerUI to test this

--- account-service/src/main/java/finos/traderx/accountservice/model/Account.java
+++ account-service/src/main/java/finos/traderx/accountservice/model/Account.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -17,7 +18,9 @@ public class Account implements Serializable {
 
 		@Id
 		@Column(name = "ID")
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
+		@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="account_generator")
+		@SequenceGenerator(name = "account_generator", sequenceName = "ACCOUNTS_SEQ", allocationSize = 1)
+
 		private int id;
 
 		@Column(length = 50, name = "DisplayName")
