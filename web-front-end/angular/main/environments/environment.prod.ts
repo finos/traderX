bceb86f037e9202e9ade8fffce85688e199a0315
DovOps <4614730+DovOps@users.noreply.github.com>
2024-02-15 14:20:17 +0000

    Added angular configs that work both locally and via ingress, with ssl and without

--- web-front-end/angular/main/environments/environment.prod.ts
+++ web-front-end/angular/main/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: true,
-    accountUrl: 'https://' + window.location.hostname + '/account-service',
-    refrenceDataUrl: 'https://' + window.location.hostname + '/reference-data',
-    tradesUrl: 'https://' + window.location.hostname + '/trade-service/trade/',
-    positionsUrl: 'https://' + window.location.hostname + '/position-service',
-    peopleUrl: 'https://' + window.location.hostname + '/people-service/people',
-    tradeFeedUrl: 'https://' + window.location.hostname
+    production:         true,
+    accountUrl:         `//${window.location.host}/account-service`,
+    refrenceDataUrl:    `//${window.location.host}/reference-data`,
+    tradesUrl:          `//${window.location.host}/trade-service/trade/`,
+    positionsUrl:       `//${window.location.host}/position-service`,
+    peopleUrl:          `//${window.location.host}/people-service/people`,
+    tradeFeedUrl:       `//${window.location.host}`
 };
