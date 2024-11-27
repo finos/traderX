5386e717eb5feebbbed78ffc702bd7265c7263bd
kwaw.amihere <kwaw.amihere@turntabl.io>
2024-11-27 11:00:37 +0000

    include new path for processor

--- web-front-end/angular/main/environments/environment.prod.ts
+++ web-front-end/angular/main/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     accountUrl:         `//${window.location.host}/account-service`,
     refrenceDataUrl:    `//${window.location.host}/reference-data`,
     tradesUrl:          `//${window.location.host}/trade-service/trade/`,
+    tradeProcessorUrl:  `//${window.location.host}/tradeservice`,
     positionsUrl:       `//${window.location.host}/position-service`,
     peopleUrl:          `//${window.location.host}/people-service`,
     tradeFeedUrl:       `//${window.location.host}`
